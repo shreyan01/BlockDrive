93a1fad88312cb911afb3435956fbf8b1ee85493
shreyan01 <shreyanshukla19@gmail.com>
2024-05-20 22:41:44 +0530

    Created landingpage.js & landingpage.css and updated App.js

---
+++ client/src/components/landingpage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './landingpage.css'
+const LandingPage=()=>{
+    return (
+        <div className="landing-page">
+            <header>
+                <h1>
+                    Welcome to BlockDrive
+                </h1>
+            </header>
+            <main>
+                <p>Store and manage your files efficiently and securely on our blockchain!</p>
+                <h3>Personal Blockchain specially for you</h3>
+                <nav>
+                    <ul>
+                        <li><a href="/signup">Sign Up</a></li>
+                        <li><a href="/login">Log In</a></li>
+                        <li><a href="/drive">Access Your Drive</a></li>
+                    </ul>
+                </nav>
+            </main>
+        </div>
+    )
+}

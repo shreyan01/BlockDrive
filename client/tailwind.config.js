a208129faafcf6c6ea256cac361072f12084c13a
shreyan01 <shreyanshukla19@gmail.com>
2024-06-22 01:52:01 +0530

    updated frontend color tropes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -17,11 +17,11 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#ec4755',
+        primary: '#40dfaf',
         secondary: '#a12c34',
         tertiary: '#99a0a3',
         border: '#1a2e35',
-        background: '#ffffff',
+        background: '#e6e6dd',
       },
       animation: {
         vote: 'vote 1s ease-in-out',

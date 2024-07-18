da4dfeac3ece7501e3199b5a8047f480a7370e7f
shreyan01 <shreyanshukla19@gmail.com>
2024-07-18 19:51:17 +0530

    resolved routing issues

---
+++ client/src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../components/Dashboard';
+
+const Dashboard1: React.FC = () => {
+  return (
+    <div>
+      <Dashboard/>
+    </div>
+  );
+};
+
+export default Dashboard1;

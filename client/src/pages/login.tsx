64f0d6bbfacfb8173b2d1bdda4330b5369dfa066
shreyan01 <shreyanshukla19@gmail.com>
2024-07-18 21:58:51 +0530

    created accessible pages for login and protected route

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,13 @@
+// pages/login.tsx
+import Auth from '../components/Auth';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <Auth />
+    </div>
+  );
+};
+
+export default Login;

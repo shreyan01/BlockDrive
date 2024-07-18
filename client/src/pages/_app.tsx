e921c3eedb3e6a71aa6398ca3c62901d2c0e391f
shreyan01 <shreyanshukla19@gmail.com>
2024-07-18 20:58:40 +0530

    added supabase for authentication

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { AppProps } from 'next/app';
-
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import '../styles/main.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps<{initialSession: Session}>){
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+  return(
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
+};
 
 export default MyApp;

5656aee69946104399b17aaf39e2f23cc294c46e
shreyan01 <shreyanshukla19@gmail.com>
2024-07-24 17:00:22 +0530

    improved Search bar UI

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -11,17 +11,17 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="flex items-center bg-gray-100 rounded-xl pr-3 w-80">
+    <div className="flex items-center bg-gray-100 rounded-2xl pr-3 w-80">
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Search..."
-        className="w-full bg-transparent border-none rounded-xl m-2 p-2 focus:outline-none focus:ring-2 focus:ring-primary"
+        className="w-full bg-transparent border-none rounded-2xl m-2 p-2 focus:outline-none focus:ring-2 focus:ring-primary"
       />
       <button
         onClick={handleSearch}
-        className="text-gray-500 hover:text-gray-700 transition duration-300 bg-primary rounded-xl p-2"
+        className="text-gray-500 hover:text-gray-700 transition duration-300 bg-primary rounded-2xl p-2"
       >
         <FiSearch size={24} color='white' />
       </button>

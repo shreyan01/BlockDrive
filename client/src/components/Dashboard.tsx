feec67b1ea04fce3986a2cda4eba1a41f1dc9034
shreyan01 <shreyanshukla19@gmail.com>
2024-08-12 23:52:28 +0530

    Added file block to dashboard

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import DashNavbar from './dashboardNavbar';
 import Sidebar from './Sidebar';
+import FileTemplate from './FileTemplate';
 
 const Dashboard: React.FC = () => {
   return (
     <div className='flex flex-col bg-background h-screen'>
       <DashNavbar/>
+      <div className='flex flex-row'>
       <Sidebar/>
+      <FileTemplate/>
+      </div>
     </div>
   );
 };

f2b1567fce2edecc452a8e8a844e072d48ff7233
shreyan01 <shreyanshukla19@gmail.com>
2024-08-22 18:52:21 +0530

    added the access to the modal to upload file button

--- client/src/components/dropdown.tsx
+++ client/src/components/dropdown.tsx
@@ -1,13 +1,16 @@
 import React, {useState} from "react"
 import { FiPlus, FiFolder, FiFilePlus, FiFolderPlus } from "react-icons/fi";
+import UploadModal from "./UploadModal";
 const DropDown=()=>{
     const [isOpen, SetIsOpen]=useState(false);
     const toggleDropDown=()=>{
         SetIsOpen(!isOpen);
     }
-    const handleUpload=()=>{
-        
-    }
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => {
+    setIsModalOpen(false);
+    };
     return(
         <div className="flex flex-col items-center bg-background1 shadow-md w-2/3 m-8 rounded-lg hover:bg-background">
             <button onClick={toggleDropDown} className="bg-transparent inline-flex justify-center w-full items-center flex-row h-10 rounded text-primary"><FiPlus color="#40dfaf"/> New</button>
@@ -24,7 +27,8 @@ const DropDown=()=>{
                     <button className="p-2">Folder</button>
                     </div>
                     <div className="px-4 py-2 text-sm text-primary hover:bg-background flex flex-row items-center justify-center">
-                        <FiFilePlus/><button onClick={handleUpload} className="p-2">Upload File</button>
+                        <FiFilePlus/><button onClick={openModal} className="p-2">Upload File</button>
+                        <UploadModal isOpen={isModalOpen} closeModal={closeModal} />
                     </div>
                     <a
                     href="#"

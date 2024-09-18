a57ba58202ad82441ec7a30e97affb88d6eae2ac
shreyan01 <shreyanshukla19@gmail.com>
2024-09-18 23:57:32 +0530

    imported waitlisting to make a page

---
+++ client/src/pages/waiting.tsx
@@ -0,0 +1,12 @@
+import Waitlist from "../components/waitlist";
+import React from 'react'
+
+const Onboarding: React.FC=()=>{
+    return(
+        <div>
+            <Waitlist/>
+        </div>
+    )
+}
+
+export default Onboarding

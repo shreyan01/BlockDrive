b6b47338d9f54cbe933543be80785dd271ddee63
shreyan01 <shreyanshukla19@gmail.com>
2024-09-18 23:57:10 +0530

    made waitlisting component

---
+++ client/src/components/waitlist.tsx
@@ -0,0 +1,9 @@
+const Waitlist = () => {
+    return(
+        <div className="bg-background">
+            <button className="flex items-center text-center align-middle justify-center bg-primary text-white">Join The Waitlist</button>
+        </div>
+    )
+}
+
+export default Waitlist

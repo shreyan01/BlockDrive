db5769e69df9115ca2b779b37c1c03fda61d62dc
shreyan01 <shreyanshukla19@gmail.com>
2024-09-26 14:32:02 +0530

    added function to add the filename upon upload

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react'; // Import useState
 import DashNavbar from './dashboardNavbar';
 import Sidebar from './Sidebar';
 import FileTemplate from './FileTemplate';
 
 const Dashboard: React.FC = () => {
+  const [fileName, setFileName] = useState<string>(''); // State for file name
+
+  // Function to handle file upload and set the file name
+  const handleFileUpload = (name: string) => {
+    setFileName(name);
+  };
+
   return (
     <div className='flex flex-col bg-background h-screen'>
       <DashNavbar/>
       <div className='flex flex-row'>
-      <Sidebar/>
-      <FileTemplate/>
+        <Sidebar onFileUpload={handleFileUpload} /> {/* Pass the handler to Sidebar */}
+        <FileTemplate fileName={fileName} /> {/* Pass fileName as prop */}
       </div>
     </div>
   );
